9932e30388a5a6b75c373545d9d1bb823aff5fe5
Ryan Brady <ryan.m.brady@stonybrook.edu>
2024-07-05 13:00:16 -0400

    whitespaces

--- Source/hydro/advection_util.cpp
+++ Source/hydro/advection_util.cpp
@@ -57,7 +57,6 @@ Castro::ctoprim(const Box& bx,
   });
 }
 
-
 void
 Castro::shock(const Box& bx,
               Array4<Real const> const& q_arr,
@@ -131,9 +130,7 @@ Castro::shock(const Box& bx,
 #endif
     }
 
-if (castro::disable_shock_burning == 0) {
-  shk(i,j,k) = 0.0_rt;
-} else if (castro::disable_shock_burning == 1) {
+if (castro::disable_shock_burning == 1) {
     // This is a basic multi-dimensional shock detection algorithm.
     // we look for |grad P . dx| / P > 2/3 and div u < 0
     // This is basically the method in Gronow et al. 2020
